dcca9d78e6c24edc859fd28c89952ed79c90491c
geraldlcpd <geraldlcpd@users.noreply.github.com>
2018-04-10 00:03:08 +0800

    Prac_Prep for Merge PyCharm

--- practices/practice_program.py
+++ practices/practice_program.py
@@ -1,2 +1,9 @@
-words = ["aye", "bee", "sea", "tea"]
-print(words.pop())
+ages_dict =  {'Bill':21, 'Jane':34, 'Jack':56}
+name_input = input("Enter Name : >> ")
+age_input = input("Enter Age: >> ")
+
+ages_dict.update({name_input:age_input})
+num_keys = len(ages_dict)
+
+for key, value in ages_dict.items():
+    print("{:8} - {:>6j}".format(key, value))
