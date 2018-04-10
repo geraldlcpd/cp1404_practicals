4b753e93d92eeed2ec0e422fa27b68e8fbc0421b
geraldlcpd <geraldlcpd@users.noreply.github.com>
2018-04-10 11:11:33 +0800

    Refining words count program and removing unwanted variables from Practical 5 exercises

--- prac_05/words_count.py
+++ prac_05/words_count.py
@@ -6,17 +6,18 @@ text = input("Please enter phrase : > ")
 words = []
 word_count = {}
 i = 0
-j = 0
+
 words = text.split()
+words.sort()
+
 w_ct = len(words)
 for i in range(0, w_ct-1, 1):
     word_count[words[i]] = 0
 
-x = 0
+i = 0
 for element in words:
-    if words[x] in word_count:
-        word_count[words[x]] += 1
-    x = x + 1
-
+    if words[i] in word_count:
+        word_count[words[i]] += 1
+    i = i + 1
 for key, value in word_count.items():
     print("{:12} : {:3}".format(key, value))
