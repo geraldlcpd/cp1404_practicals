732a4af1f18010437ff7e668ce9ff8b908add8b2
geraldlcpd <geraldlcpd@users.noreply.github.com>
2018-05-12 13:03:41 +0800

    Finishing Touch for Prac_06, trying out change font

--- prac_06/mi_to_km.py
+++ prac_06/mi_to_km.py
@@ -13,16 +13,49 @@ class MiToKm(App):
         return self.root
 
     def increment_up(self, value):
-        result_up = value + 1
+        try:
+            float(value)
+        except ValueError:
+            value = 0.0
+            self.root.ids.input_mi.text = 'Invalid'
+
+        number = float(value)
+        result_up = number + 1
         self.root.ids.input_mi.text = str(result_up)
 
     def increment_down(self, value):
-        result_down = value - 1
-        self.root.ids.input_mi.text = str(result_down)
+        valid_input = False
+        if valid_input == False:
+            try:
+                float(value)
+                valid_input = True
+            except ValueError:
+                pass
+        if valid_input == True:
+            number = float(value)
+            if number <= 0.0:
+                result_down = 0.0
+                self.root.ids.input_mi.text = str(result_down)
+            else:
+                result_down = number - 1
+                self.root.ids.input_mi.text = str(result_down)
+
 
     def handle_calculate(self, value):
-        result_calculate = value * 1.61
-        result_disp = '{:.2f} mi = {:.2f} km'.format(value, result_calculate)
-        self.root.ids.output_label.text = result_disp
+        valid_input = False
+        if valid_input == False:
+            try:
+                float(value)
+                valid_input = True
+            except ValueError:
+                pass
 
+        if valid_input == True:
+            number = float(value)
+            result_calculate = number * 1.61
+            result_display = '{:.2f} mi = {:.2f} km'.format(number, result_calculate)
+            self.root.ids.output_label.text = result_display
+        else:
+            result_display = '-.-- mi = -.-- km'
+            self.root.ids.output_label.text = result_display
 MiToKm().run()
