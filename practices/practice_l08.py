9e3b99a95c73a9f02e037833ae2f9951fe49dc84
Gerald Oswald Kalendesang <gerald.kalendesang@my.jcu.edu.au>
2018-05-22 11:50:05 +0800

    Change Client PC >> macOS

--- practices/practice_l08.py
+++ practices/practice_l08.py
@@ -1,5 +1,20 @@
-class Person:
-    def __init__(self, name='', age=0):
-        student_id = ''
+evaluate = (3 >= 3) or (5 < 7) and (9 != 5)
+print(evaluate)
 
-class Student(Person):
+eval2 = 9 % 5
+print(eval2)
+
+print(" ".join([str(i * 2) for i in range(1, 16, 3) if i % 2 == 0]))
+
+values = (2, 4, 6)
+for x, y in enumerate(values):
+    print("{0}:{1:.0f}".format(x, y), end=" ")
+
+
+def rec_fun(n):
+    if n > 0:
+        return rec_fun(n - 1) + n * 2
+    return 0
+
+
+print('\n', rec_fun(4))
